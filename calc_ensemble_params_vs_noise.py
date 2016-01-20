81af0d59f6e92fce7973b86093e6851cefb8b70c
rustyBilges <tophermcw@gmail.com>
2016-01-20 15:35:22 +0000

    All calculator scripts wokring and tested locally. Now to set running on BC3

--- calc_ensemble_params_vs_noise.py
+++ calc_ensemble_params_vs_noise.py
@@ -5,7 +5,7 @@
 
 from datetime import datetime
 from configure import NOISE_VALUE, NUMBER_OF_PARAMETERS, PARAMETER_FILE, DT, REPEATS_PER_PARAMETER_SET, NUMBER_OF_SAMPLES, NUMBER_OF_BINS
-from linear_simulator import linear_simulator
+from hollingII_simulator import hollingII_simulator
 from sampler import sampler
 from timme_calculator import timme_calculator
 import numpy as np
@@ -15,11 +15,11 @@ import os, sys
 ##configure:
 PARAMETER_FILE = './parameter_log.txt' 
 
-REPEATS_PER_PARAMETER_SET = 10
+REPEATS_PER_PARAMETER_SET = 1
 DT = 0.0001
 NUMBER_OF_SAMPLES = 10000
 
-params = range(100) 
+params = range(10) ## change!!
 printout = False
 plot_dynamics = False
 ##################################
@@ -27,7 +27,7 @@ plot_dynamics = False
 start_sim = datetime.now()
 parameters = np.genfromtxt(PARAMETER_FILE, delimiter=',')
 
-noise = range(100) 
+noise = range(10)  ## change!! 
 
 RESULTS = np.zeros((len(noise), 1 + (6 + 2)*2))  ## as below. *2 for mean and variance over repeats.
 RESULTS[:,0] = noise
@@ -40,6 +40,7 @@ for ni in noise:
     summary_array = np.zeros((Nparams * REPEATS_PER_PARAMETER_SET, 6 + 2))  # to store results for each simulation, used to calculate mean and variance of estimates
 					               			    ## a0 | J00 | J01 | a1 | J10 | J11 | err1 | err2
 									    ## relative error in all of the above, excepting J11, err1, err2		 
+									    ## therefore extra +4 not required as in single param calculators
     sa_id = 0
     for pi in params:    
     	
@@ -48,12 +49,13 @@ for ni in noise:
 		a = parameters[pi,1]
 		b = parameters[pi,2]
 		c = parameters[pi,3]
+		d = parameters[pi,4]
 		
-		x00 = parameters[pi,13]
-		x10 = parameters[pi,14]
-		T2P = parameters[pi,12]
+		x00 = parameters[pi,14]
+		x10 = parameters[pi,15]
+		T2P = parameters[pi,13]
 
-		ls = linear_simulator(a, b, c, x00, x10, DT, T2P, ni, plot_dynamics)
+		ls = hollingII_simulator(a, b, c, d, x00, x10, DT, T2P, ni, plot_dynamics)
 		ls.run()
 		D = ls.get_dynamics()
 		
@@ -69,13 +71,23 @@ for ni in noise:
 		results = results[0:6]
 		results = np.append(results, err[0])
 		results = np.append(results, err[1])
+
+        	x0 = D[1,:]  # prey time series
+	        x1 = D[2,:]  # prey time series
+        	L  = np.ones(len(x0))
+
+	        a00 = np.mean(-a*L + b*x1/((x0+d)**2))   ## equala to \alpha_{00}
+        	a01 = np.mean(-b/(x0+d) )                ## equala to \alpha_{00}
+	        a10 = np.mean(c*d/((x0+d)**2))           ## equala to \alpha_{00}
+        	a11 = 0                                  ## equala to \alpha_{11} !!
+
 		## calculate relative error:
 		re = []
 		re.append(np.abs((results[0]-a)/a))
-		re.append(np.abs((results[1]+a)/a))
-		re.append(np.abs((results[2]+b)/b))
+		re.append(np.abs((results[1]-a00)/a00))
+		re.append(np.abs((results[2]-a01)/a01))
 		re.append(np.abs((results[3]+1)/1.0))
-		re.append(np.abs((results[4]-c)/c))
+		re.append(np.abs((results[4]-a10)/a10))
 		re.append(np.abs(results[5]))
 		re.append(np.abs(results[6]))
 		re.append(np.abs(results[7]))
@@ -95,7 +107,7 @@ for ni in noise:
     RESULTS[nid,1:] = np.append(np.mean(summary_array,0), np.var(summary_array,0))
     nid += 1
 
-np.savetxt('ensemble_params_vs_noise_nsamples_%d_dt_%f_reps_%d.results' %(NUMBER_OF_SAMPLES, DT, REPEATS_PER_PARAMETER_SET), RESULTS, delimiter=',')
+np.savetxt('ensemble_params_vs_noise_nsamples_%d_dt_%f_reps_%d.HII.results' %(NUMBER_OF_SAMPLES, DT, REPEATS_PER_PARAMETER_SET), RESULTS, delimiter=',')
     
 stop_sim = datetime.now()
 elapsed_sim = stop_sim-start_sim
--- calc_ensemble_params_vs_nsamples.py
+++ calc_ensemble_params_vs_nsamples.py
@@ -6,7 +6,7 @@
 
 from datetime import datetime
 from configure import NOISE_VALUE, NUMBER_OF_PARAMETERS, PARAMETER_FILE, DT, REPEATS_PER_PARAMETER_SET, NUMBER_OF_SAMPLES, NUMBER_OF_BINS
-from linear_simulator import linear_simulator
+from hollingII_simulator import hollingII_simulator
 from sampler import sampler
 from timme_calculator import timme_calculator
 import numpy as np
@@ -16,11 +16,11 @@ import os, sys
 ##configure:
 PARAMETER_FILE = './parameter_log.txt'
 
-REPEATS_PER_PARAMETER_SET = 10
+REPEATS_PER_PARAMETER_SET = 1
 DT = 0.0001
 noise = 10
 
-params = range(100)
+params = range(10)  ## change!!
 printout = False
 plot_dynamics = False
 ##################################
@@ -34,18 +34,19 @@ Nparams = len(params)
 #samples = range(50,5050,50)
 #samples = np.logspace(2,15,num=14,base=2)
 #samples = samples.astype(int)  
-#samples = [10, 20, 50, 100, 1000, 10000, 50000]
-samples = range(4,100)
-samples.append(200)
-samples.append(300)
-samples.append(400)
-samples.append(500)
-samples.append(600)
-samples.append(700)
-samples.append(800)
-samples.append(900)
-samples.append(1000)
-samples.append(10000)
+samples = [10, 20, 50, 100, 1000, 10000] #, 50000] change!!
+
+#samples = range(4,100)
+#samples.append(200)
+#samples.append(300)
+#samples.append(400)
+#samples.append(500)
+#samples.append(600)
+#samples.append(700)
+#samples.append(800)
+#samples.append(900)
+#samples.append(1000)
+#samples.append(10000)
 
 RESULTS = np.zeros((len(samples), 1 + (6 + 2)*2))  ## as below. *2 for mean and variance over repeats.
 RESULTS[:,0] = samples
@@ -63,12 +64,13 @@ for si in samples:
 		a = parameters[pi,1]
 		b = parameters[pi,2]
 		c = parameters[pi,3]
+		d = parameters[pi,4]
 		
-		x00 = parameters[pi,13]
-		x10 = parameters[pi,14]
-		T2P = parameters[pi,12]
+		x00 = parameters[pi,14]
+		x10 = parameters[pi,15]
+		T2P = parameters[pi,13]
 	       
-		ls = linear_simulator(a, b, c, x00, x10, DT, T2P, noise, plot_dynamics)
+		ls = hollingII_simulator(a, b, c,d, x00, x10, DT, T2P, noise, plot_dynamics)
 		ls.run()
 		D = ls.get_dynamics()
 		
@@ -84,16 +86,26 @@ for si in samples:
 		results = results[0:6]
 		results = np.append(results, err[0])
 		results = np.append(results, err[1])
-		## calculate relative error:
-		re = []
-		re.append(np.abs((results[0]-a)/a))
-		re.append(np.abs((results[1]+a)/a))
-		re.append(np.abs((results[2]+b)/b))
-		re.append(np.abs((results[3]+1)/1.0))
-		re.append(np.abs((results[4]-c)/c))
-		re.append(np.abs(results[5]))
-		re.append(np.abs(results[6]))
-		re.append(np.abs(results[7]))
+
+                x0 = D[1,:]  # prey time series
+                x1 = D[2,:]  # prey time series
+                L  = np.ones(len(x0))
+
+                a00 = np.mean(-a*L + b*x1/((x0+d)**2))   ## equala to \alpha_{00}
+                a01 = np.mean(-b/(x0+d) )                ## equala to \alpha_{00}
+                a10 = np.mean(c*d/((x0+d)**2))           ## equala to \alpha_{00}
+                a11 = 0                                  ## equala to \alpha_{11} !!
+
+                ## calculate relative error:
+                re = []
+                re.append(np.abs((results[0]-a)/a))
+                re.append(np.abs((results[1]-a00)/a00))
+                re.append(np.abs((results[2]-a01)/a01))
+                re.append(np.abs((results[3]+1)/1.0))
+                re.append(np.abs((results[4]-a10)/a10))
+                re.append(np.abs(results[5]))
+                re.append(np.abs(results[6]))
+                re.append(np.abs(results[7]))
 
 		summary_array[sa_id,:] = re ## saving relative error
 		sa_id += 1
@@ -110,7 +122,7 @@ for si in samples:
     RESULTS[sid,1:] = np.append(np.mean(summary_array,0), np.var(summary_array,0))
     sid += 1
 
-np.savetxt('ensemble_params_vs_nsamples_noise_%f_dt_%f_reps_%d.results' %(noise, DT, REPEATS_PER_PARAMETER_SET), RESULTS, delimiter=',')
+np.savetxt('ensemble_params_vs_nsamples_noise_%f_dt_%f_reps_%d.HII.results' %(noise, DT, REPEATS_PER_PARAMETER_SET), RESULTS, delimiter=',')
     
 stop_sim = datetime.now()
 elapsed_sim = stop_sim-start_sim
